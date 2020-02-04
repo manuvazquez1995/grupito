9a2e5cf10c9125e457c74bd75b7de4570c4f04d0
manuvazquez1995 <60600016+manuvazquez1995@users.noreply.github.com>
2020-02-04 16:04:32 +0100

    Version 3

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
   <!-- Main jumbotron for a primary marketing message or call to action -->
   <div class="jumbotron">
     <div class="container">
-      <h1 class="display-3">Bienvenido a Mi Gupito!</h1>
+      <h1 class="display-3">Bienvenido a Mi Grupito!</h1>
       <p >La tienda con las mejores ofertas de internet que podrás compartir con tu amigos.</p>
       <p><a class="btn btn-primary btn-lg" href="#" role="button">Nuestras ofertas »</a></p>
     </div>
@@ -29,8 +29,8 @@
       <div class="card-body">
         <h5 class="card-title"><?php echo $producto['nombre']; ?></h5>
         <p class="card-text"><?php echo $producto['introDescripcion']; ?></p>
-		<a href="#" class="btn btn-success">Ahora <?php echo $producto['precioOferta']; ?></a>
-		<span class="card-text text-danger float-right"><del>Antes <?php echo $producto['precio']; ?><del></span>
+		<a href="#" class="btn btn-success">Ahora <?php echo $producto['precioOferta']; ?>€</a>
+		<span class="card-text text-danger float-right"><del>Antes <?php echo $producto['precio']; ?>€<del></span>
       </div>
     </div>
   </div>
